84ae5bb8898bdd3a3e367841666fc20e4f8bedb0
Reed S. MacConnell <learn22ruby@gmail.com>
2013-08-03 12:33:10 -0400

    can't seem to get all of my files to add at once as I'm intending

--- Roman.rb
+++ Roman.rb
@@ -1,9 +1,48 @@
-I = 1
-V = 5
-X = 10
-L = 50
-C = 100
-D = 500
-M = 1000
+def old_roman_numeral num 
+	roman = ''
+	roman = roman + 'M' * (num        /1000)
+	roman = roman + 'D' * (num % 1000 / 500)
+	roman = roman + 'C' * (num %  500 / 100)
+	roman = roman + 'L' * (num %  100 /  50)
+	roman = roman + 'X' * (num %   50 /  10)
+	roman = roman + 'V' * (num %   10 /   5)
+	roman = roman + 'I' * (num %    5 /   1)
+	roman
+end
+puts
+puts(old_roman_numeral(1999))
 
-puts I
+def roman_numeral num
+	tho = (num / 1000)
+	hun = (num % 1000 / 100)
+	ten = (num % 100 /10)
+	one = (num % 10 )
+
+	roman = 'M' * tho
+	if hun == 9
+		roman = roman + 'CM'
+	elsif hun == 4
+		roman = roman + 'CD'
+	else
+		roman = roman + 'D' * (num % 1000/500)
+		roman = roman + 'C' * (num % 500/100)
+	end
+	if ten == 9
+		roman = roman = 'XC'
+	elsif ten == 4
+		roman = roman + 'XL'
+	else
+		roman = roman + 'L' * (num % 100/50)
+		roman = roman + 'X' * (num % 50/10)
+	end
+	if one == 9
+		roman = roman + 'IX'
+	elsif one == 4
+		roman = roman + 'IV'
+	else
+		roman = roman + 'V' * (num % 10/5)
+		roman = roman + 'I' * (num % 5/1)
+	end
+	roman
+end
+puts(roman_numeral(1999))
